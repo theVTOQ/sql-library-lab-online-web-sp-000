c4cc06005d74bf372f027347c3f8e063d153c003
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-10 19:49:37 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species)
   FROM characters
   GROUP BY species
-  HAVING MAX(COUNT(species))"
+  ORDER BY COUNT(species);"
 end
 
 def select_name_and_series_subgenres_of_authors

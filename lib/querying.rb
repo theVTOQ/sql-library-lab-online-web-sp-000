96aa4b07fcdf302f00c998c2aaf4066a4899c656
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-10 23:49:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   WHERE series_id = 1
   ORDER BY year;"
 end
-
++
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto
   FROM characters

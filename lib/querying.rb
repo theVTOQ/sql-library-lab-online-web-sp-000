cc8d020a7cba6fff27dba3940a2029d91182ed88
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-10 19:54:51 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,10 +22,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.subgenre_id
+  "SELECT authors.name, subgenres.name
   FROM authors
   INNER JOIN series
-  ON authors.id = series.author_id;
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON subgenres.id = series.subgenre_id;
   "
 end
 
@@ -35,8 +37,7 @@ def select_series_title_with_most_human_characters
   INNER JOIN characters
   ON series.id = characters.series_id
   WHERE characters.species = 'human'
-  GROUP BY series.title
-  HAVING MAX(COUNT(characters.species));"
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in

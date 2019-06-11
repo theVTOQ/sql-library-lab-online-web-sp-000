d44d74a4eaf6d78df27e25c5ebd3509fc5ec1235
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-06-11 00:00:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,7 +47,7 @@ def select_character_names_and_number_of_books_they_are_in
   INNER JOIN character_books
   ON characters.id = character_books.character_id
   JOIN books
-  ON books.series_id = character_books.book_id
+  ON books.id = character_books.book_id
   GROUP BY characters.name;
   "
 end
